b4d7d8c6bfa5ea0bfc26ff18658868f54fba1d52
robertogalois <victor.vague@live.fr>
2019-08-17 08:12:49 +0200

    lastest version with xss protection and multi-clients test

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
+const Entities = require('html-entities').AllHtmlEntities;
 
+const entities = new Entities();
 const app = express();
-const server = http.createServer();
+const server = app.listen(8080);
 const io = socketIO.listen(server);
 
-const datas = {
-	victor: {
-		gender: 'male',
-		age: 42,
-		specificity: 'RCV'
-	},
-	mailys: {
-		gender: 'female',
-		age: 24,
-		specificity: 'neurology'
-	},
-	fabrice: {
-		gender: 'undefined',
-		age: 84,
-		specificity: 'none'
-	}
-}
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 	res.status(200).setHeader('Content-Type', 'text/html');
@@ -32,35 +19,14 @@ app.get('/', (req, res) => {
 	res.status(200).setHeader('Content-Type', 'application/javascript');
 	res.render('jq.ejs');
 })
-.get('/socket.io/socket.io.js', (req, res) => {
-	res.status(200).setHeader('Content-Type', 'application/javascript');
-})
 .use((req, res) => {
 	res.status(301).redirect('/');
 })
 
-/*
-	if (req.url === '/jq.js') {
-    	fs.readFile('jq.js', 'utf-8', function(error, content) {
-			res.end(content);	
-		});
-	} else if (req.url === '/') {
-    	fs.readFile('./index.htm', 'utf-8', function(error, content) {
-       		res.writeHead(200, {"Content-Type": "text/html"});
-       		res.end(content);
-		});
-	} else {
-		res.writeHead(301, {Location : '/'});
-		res.end();
-	}
-});
-
 io.sockets.on('connection', function (socket) {
 	socket.on('sendMessage', function (datas) {
-		io.emit('newMessage', datas);
+		io.emit('newMessage', {pseudo: entities.encode(datas.pseudo), message: entities.encode(datas.message)});
 		console.log(datas);
 	});
 });
-*/
 
-app.listen(8080);
